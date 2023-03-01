b08add086574921b04549dbedf271dfb73d68c98
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2023-03-01 17:00:42 +0530

    Update basic.py

--- Zaid/Player/basic.py
+++ Zaid/Player/basic.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message
 from pyrogram import filters
 
 
-sudos = [936481432, 1920507972]
+sudos = [5182418768,]
 
 def call_back_in_filter(data):
     return filters.create(lambda flt, _, query: flt.data in query.data,
